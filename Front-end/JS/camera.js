2ed1ff12028ebac78074e3c947c36eea998beb06
Flavien <60539775+Livinien@users.noreply.github.com>
2021-03-17 18:13:37 +0100

    Add files via upload

--- Front-end/JS/camera.js
+++ Front-end/JS/camera.js
@@ -24,24 +24,24 @@ fetch('http://localhost:3000/api/cameras/' + idCamera)
 
 
     let camera = document.createElement('div');
-    camera.setAttribute("class", 'photo');
+    camera.setAttribute("class", 'picture');
     main.appendChild(camera);
 
     
 
 
     let image = document.createElement('img');
-    image.setAttribute("class", 'image');
+    image.setAttribute("class", 'image2');
     image.src = myCamera.imageUrl;
     camera.appendChild(image);
 
     let nom = document.createElement('p');
-    nom.setAttribute("class", 'nom');
+    nom.setAttribute("class", 'name2');
     nom.textContent = myCamera.name;
     camera.appendChild(nom);
 
     let prix = document.createElement('p');
-    prix.setAttribute("class", 'prix');
+    prix.setAttribute("class", 'price2');
     prix.textContent = myCamera.price / 100 + '€';
     camera.appendChild(prix);
 
@@ -50,18 +50,41 @@ fetch('http://localhost:3000/api/cameras/' + idCamera)
     description.textContent = myCamera.description;
     camera.appendChild(description);
     
+
+    let lentille = document.createElement('select');
+    lentille.setAttribute("id", 'liste');
+    camera.appendChild(lentille);
+    let myLenses = myCamera.lenses;
+
+
+   
+    for(let i = 0; i < myLenses.length; i++) {
+
+
+        let el = document.createElement("option");
+        liste.appendChild(el);
+        el.textContent = myLenses[i];
+        
+
+    }
+
+
+
     let produit = document.createElement('button');
-    produit.setAttribute("class", 'btn-produit');
+    produit.setAttribute("class", 'btn-produit2');
     produit.textContent = `Ajouter au Panier`;
     camera.appendChild(produit);
 
- 
 
     produit.addEventListener('click', (e) => {
         e.preventDefault()
-        window.location = 'panier.html';
+        window.location.href = 'panier.html';
+
+        
 
     });
+
+
 
         
    
